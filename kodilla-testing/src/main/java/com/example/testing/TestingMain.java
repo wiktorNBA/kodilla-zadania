9e40b3d9e361668ec1cbe9e04551073aab43a045
Wiktor Woźniakowski <wiktorwoz21@gmail.com>
2023-04-18 18:59:09 +0200

    6.2 Calculator

--- kodilla-testing/src/main/java/com/example/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/example/testing/TestingMain.java
@@ -1,7 +1,23 @@
-package com.kodilla.testing;
+package com.example.testing;
+
+import com.example.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator result1 = new Calculator();
+        Calculator result2 = new Calculator();
+
+        if (result1.add(1, 2) == 3) {
+            System.out.println("Add test: OK");
+        } else {
+            System.out.println("Add test: Error!");
+        }
+
+        if (result2.subtract(5, 3) == 2) {
+            System.out.println("Subtract test: OK");
+        } else {
+            System.out.println("Subtract test: Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/example/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.testing.calculator;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+}
+
