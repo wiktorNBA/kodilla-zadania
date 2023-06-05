dc1ec6d93689b1dd210230db04926b53a674db4d
Wiktor Woźniakowski <wiktorwoz21@gmail.com>
2023-06-05 19:02:50 +0200

    13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/kodillagoodpatterns/challenges/Application.java
@@ -0,0 +1,24 @@
+package com.kodilla.kodillagoodpatterns.challenges;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class Application {
+    public static void main(String[] args){
+        MovieStore movieStore = new MovieStore();
+        Map<String, List<String>> movieMap = movieStore.getMovies();
+
+
+
+        final String movies = movieMap.entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.println(movies);
+
+
+    }
+}
