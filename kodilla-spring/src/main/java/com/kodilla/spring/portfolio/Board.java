8b79410edb82ab53aed7ff16518fe90bfabec562
Wiktor <wiktorwoz21@gmail.com>
2023-06-22 14:21:13 +0200

    14.3 Without tests

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.portfolio;
+
+public class Board {
+
+    private TaskList toDoList;
+    private TaskList inProgressList;
+    private TaskList doneList;
+
+    public Board(final TaskList toDoList, final TaskList inProgressList, final TaskList doneList) {
+        this.toDoList = toDoList;
+        this.inProgressList = inProgressList;
+        this.doneList = doneList;
+    }
+
+    public String showTheBoard() {
+        return "Show the board:\n" + toDoList.getTasksList() + "\n"
+                + inProgressList.getTasksList() + "\n" + doneList.getTasksList();
+    }
+
+    public TaskList getToDoList() {
+        return toDoList;
+    }
+
+    public TaskList getInProgressList() {
+        return inProgressList;
+    }
+
+    public TaskList getDoneList() {
+        return doneList;
+    }
+}
---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,20 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+
+    List<String> tasks;
+
+    public TaskList(){
+        this.tasks=new ArrayList<>();
+    }
+
+    public List<String> getTasksList(){
+        return tasks;
+    }
+
+
+
+}
