1a13d21cf149254acbd8bbdc42f09e62bc24309b
Wiktor Woźniakowski <wiktorwoz21@gmail.com>
2023-06-22 09:50:03 +0200

    Naprawa środowiska

--- kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kodilla.kodillagoodpatterns;
+package com.kodilla.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaGoodPatternsApplicationTests {
+class KodillaSpringApplicationTests {
 
     @Test
     void contextLoads() {
