d38e11ab667effe65a8c2c4ba0da7439fd9b3e56
Wiktor Woźniakowski <wiktorwoz21@gmail.com>
2023-06-22 19:42:22 +0200

    14.1 Dodanie klasy main

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/KodillaSpringApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KodillaSpringApplicationTests {
+    @Test
+    void contextLoads(){
+
+    }
+}
