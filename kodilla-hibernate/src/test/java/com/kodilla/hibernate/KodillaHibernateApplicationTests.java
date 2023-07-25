d3020f09d70d7fd609f2c4e27e97679578c3186a
Wiktor <wiktorwoz21@gmail.com>
2023-07-25 14:28:01 +0200

    17.1

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kodilla.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class KodillaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+}
+
